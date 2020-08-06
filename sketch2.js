7558af1437266a18899958ed23b1add007520f03
Yash27112006 <64268919+Yash27112006@users.noreply.github.com>
2020-08-06 16:34:10 +0530

    Add files via upload

---
+++ sketch2.js
@@ -0,0 +1,31 @@
+//creates the variable
+var email;
+
+function preload(){
+    //loads the image 
+    backImg = loadImage("design_files/images/backImg2.PNG");
+}
+function setup(){
+    //creates the canvas
+    canvas = createCanvas(windowWidth - 20, windowHeight-30);
+
+    //creates the page for sending mail with class Mail
+    email = new Mail();
+    email.sendMail();
+
+  //creates a button to logout
+  var button3 = createButton('Back to List');
+  button3.position(1550,20);
+  button3.style('width','100px');
+  button3.style('height','30px')
+
+  //creates the function for pressing the button2
+  button3.mousePressed(()=>{
+     window.location.href = "index.html";
+  });
+
+}
+function draw(){
+    //sets the background
+    background(backImg);
+}
